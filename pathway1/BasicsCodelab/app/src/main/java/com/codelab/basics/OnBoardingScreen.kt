e1cb9b75172bf7efa19d350e993217bdaadb1e27
Jeong-heon2 <cjhgg2@gmail.com>
2022-11-20 21:17:23 +0900

    [pathway 1-1] BasicCodelab

---
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/OnBoardingScreen.kt
@@ -0,0 +1,41 @@
+package com.codelab.basics
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelab.basics.ui.theme.BasicsCodelabTheme
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        androidx.compose.material3.Text("Welcome to the Basics Codelab!")
+        androidx.compose.material3.Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            androidx.compose.material3.Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
+    }
+}
